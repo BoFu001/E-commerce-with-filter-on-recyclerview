972b7c6999846b7871e190e381c77e360e7c7ee1
bofu <bofu2013@gmail.com>
2021-04-21 13:36:57 +0200

    Starting with the test.

---
+++ app/src/main/java/com/bofu/a20210421_fu_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bofu.a20210421_fu_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
